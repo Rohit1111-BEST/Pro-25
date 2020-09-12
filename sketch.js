1118e526ce8de76acf19e194181ee7777a1fd52c
Rohit1111-BEST <65074708+Rohit1111-BEST@users.noreply.github.com>
2020-09-12 12:16:13 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,62 @@
+var  paper,paperBody;
+var  ground, dustbin;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+
+
+
+
+
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 500);
+	rectMode(CENTER);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	ground=new Ground(width/2, height-35, width,10);
+	dustbin1 = new Dustbin(420,height-50, 70,10);
+	dustbin2 = new Dustbin(460,height-80,10,50);
+	dustbin3 = new Dustbin(380,height-80,10,50);
+	paper = new Paper(200,200,20);
+
+	
+
+
+
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(255);
+  Engine.update(engine)
+
+
+  ground.display();
+  dustbin1.display();
+  dustbin2.display();
+  dustbin3.display();
+  paper.display();
+
+
+ 
+}
+
+function keyPressed() {
+
+ if (keyCode === UP_ARROW) {
+	
+    // Look at the hints in the document and understand how to make the package body fall only on
+    Matter.Body.applyForce(paper.body,paper.body.position,{x:30,y:-30});
+  }
+}
